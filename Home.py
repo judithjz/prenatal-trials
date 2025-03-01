9cc2816c6662e7db4956dae1603eba999b6eeb94
Lawrence Richer <lricher@ualberta.ca>
2025-03-01 06:00:50 -0700

    2025-03-01

--- Home.py
+++ Home.py
@@ -3,24 +3,18 @@ import psycopg2
 import pandas as pd
 import plotly.express as px
 
-# Database connection parameters
-DB_PARAMS = {
-    "host": "aact-db.ctti-clinicaltrials.org",
-    "port": 5432,
-    "dbname": "aact",
-    "user": "lricher",
-    "password": "egr.pqy!TYK0qmv2dpt"
-}
+# Access the secrets
+db_secrets = st.secrets["database"]
 
 def connect_to_database():
     """Establish connection to the AACT database."""
     try:
         conn = psycopg2.connect(
-            host=DB_PARAMS['host'],
-            port=DB_PARAMS['port'],
-            dbname=DB_PARAMS['dbname'],
-            user=DB_PARAMS['user'],
-            password=DB_PARAMS['password']
+            host=db_secrets["host"],
+            port=db_secrets["port"],
+            dbname=db_secrets["dbname"],
+            user=db_secrets["user"],
+            password=db_secrets["password"]
         )
         return conn
     except Exception as e:
@@ -29,7 +23,7 @@ def connect_to_database():
 
 def fetch_active_trials_data(conn):
     """
-    Fetch data of active trials grouped by country and sorted by the top 10 countries.
+    Fetch data of active trials grouped by country and sorted by the top 15 countries.
     """
     query = """
     SELECT 
@@ -49,6 +43,31 @@ def fetch_active_trials_data(conn):
         st.error(f"Error fetching data: {e}")
         return pd.DataFrame()
 
+def fetch_can_trials(conn):
+    """
+    Fetch data of all trials in Canada 
+    """
+    query = """
+    SELECT 
+    studies.overall_status,
+    EXTRACT(YEAR FROM studies.start_date) AS start_year,
+    COUNT(studies.nct_id) AS can_trials
+    FROM studies
+    JOIN countries ON studies.nct_id = countries.nct_id
+    WHERE countries.name = 'Canada'
+    AND EXTRACT(YEAR FROM studies.start_date) > 2010
+    AND studies.overall_status NOT IN ('COMPLETED', 'TERMINATED', 'NO_LONGER_AVAILABLE', 
+                                        'WITHDRAWN', 'APPROVED_FOR_MARKETING', 'SUSPENDED')
+    GROUP BY start_year, studies.overall_status
+    ORDER BY start_year, studies.overall_status DESC;
+    """
+    try:
+        df = pd.read_sql_query(query, conn)
+        return df
+    except Exception as e:
+        st.error(f"Error fetching data: {e}")
+    return pd.DataFrame()
+
 # Streamlit app
 def main():
     st.title("Active Clinical Trials Per Country")
@@ -59,14 +78,11 @@ def main():
     if conn:
         with conn:
             # Fetch data
-            st.info("Fetching data from the database...")
+            st.info("Fetching data from the AACT database...")
             active_trials_data = fetch_active_trials_data(conn)
             
             if not active_trials_data.empty:
-                # Display data
-                st.subheader("Top 10 Countries with Active Clinical Trials")
-                st.dataframe(active_trials_data)
-
+                
                 # Plot data with Plotly
                 st.subheader("Graph: Number of Active Trials Per Country")
                 fig = px.bar(
@@ -80,6 +96,24 @@ def main():
                 fig.update_layout(
                     yaxis=dict(autorange="reversed"),  # Reverse y-axis for highest value at the top
                     template="plotly_white",
+                )
+                st.plotly_chart(fig, use_container_width=True)
+
+                can_trials_data = fetch_can_trials(conn)
+                st.subheader("Active studies in Canada by Overall Status and Year")
+                fig = px.bar(
+                    can_trials_data,
+                    x="start_year",
+                    y="can_trials",
+                    color="overall_status",
+                    labels={"start_year": "Start Year", "can_trials": "Number of Trials", "overall_status": "Status"},
+                    title="Number of Clinical Trials by Year and Status in Canada",
+                )
+                fig.update_layout(
+                    xaxis_title="Start Year",
+                    yaxis_title="Number of Trials",
+                    barmode="group",
+                    template="plotly_white"
                 )
                 st.plotly_chart(fig, use_container_width=True)
             else:
